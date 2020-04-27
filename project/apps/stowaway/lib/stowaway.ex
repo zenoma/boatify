cee0763058f85e291ac289a49eec80705986e5fa
Francisco Pedreira Ventureira <f.pedreira@udc.es>
2020-04-27 19:30:47 +0200

    Pequeno menu Stowaway

--- project/apps/stowaway/lib/stowaway.ex
+++ project/apps/stowaway/lib/stowaway.ex
@@ -1,18 +1,43 @@
 defmodule Stowaway do
   @moduledoc """
-  Documentation for `Stowaway`.
+  Servizo de usuarios 'Stowaway' para logearse, ver viajes disponibles
+  para reservar una plaza en uno de ellos, para ver su historial de viajes,
+  y si quiere cancelar alguno.
   """
 
-  @doc """
-  Hello world.
+    #CLIENTE
 
-  ## Examples
+    def up() do
+        Process.register( spawn( fn -> menu() end), :stowclient)
+        print(0)
+    end
+    
+    def send(term), do: send(:stowclient,term)  
 
-      iex> Stowaway.hello()
-      :world
+    defp menu() do
+        receive do
+            0 ->
+                IO.puts "\nPulsa: \n 1 -Viajes disponibles \n 2 -Mis viajes \n 3 -Salir"
+                menu()            
+            1 ->  
+                IO.puts "Buscando viajes..."
+                #do (Esto sería una llamada a gen Server)
+                print(0)
+                menu()
+            2 ->
+                IO.puts "Cargando viajes..."
+                #do (Esto sería una llamada a gen Server)
+                print(0)
+                menu()                
+            3 ->
+                IO.puts "Hasta pronto!"
+                Process.unregister(:stowclient)
+                :ok
+            _msg ->
+                IO.puts "Escoge una de las opciones posibles"
+                send(:stowclient,0)
+                menu()
+        end 
+    end
 
-  """
-  def hello do
-    :world
-  end
 end
