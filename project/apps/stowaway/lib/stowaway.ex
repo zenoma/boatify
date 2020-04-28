986e890b57c52ff14ce12e67877f073d343a7351
Francisco Pedreira Ventureira <f.pedreira@udc.es>
2020-04-28 19:38:42 +0200

    Proceso cliente (Menu) completado, comienzos de Servidor con GS

--- project/apps/stowaway/lib/stowaway.ex
+++ project/apps/stowaway/lib/stowaway.ex
@@ -7,12 +7,12 @@ defmodule Stowaway do
 
     #CLIENTE
 
-    def up() do
+    def levantar_cliente() do
         Process.register( spawn( fn -> menu() end), :stowclient)
-        print(0)
+        enviar(0)
     end
     
-    def send(term), do: send(:stowclient,term)  
+    def enviar(term), do: send(:stowclient,term)  
 
     defp menu() do
         receive do
@@ -21,23 +21,59 @@ defmodule Stowaway do
                 menu()            
             1 ->  
                 IO.puts "Buscando viajes..."
-                #do (Esto sería una llamada a gen Server)
-                print(0)
+                #ver_viajes()
+                enviar(0)
                 menu()
             2 ->
                 IO.puts "Cargando viajes..."
-                #do (Esto sería una llamada a gen Server)
-                print(0)
-                menu()                
+                enviar(0)   
+                menu()          
             3 ->
                 IO.puts "Hasta pronto!"
                 Process.unregister(:stowclient)
                 :ok
             _msg ->
                 IO.puts "Escoge una de las opciones posibles"
-                send(:stowclient,0)
+                enviar(0)
                 menu()
         end 
     end
+
+
+    #SERVER
+ 
+    def levantar_servidor() do
+        GenServer.start_link(Stowaway, [], name: :stowserver)
+    end
+
+    @impl true
+    def init(smth) do 
+        {:ok, smth} 
+    end
+
+    # Call y Callback de las dos opciones del menú existente.
+    # ATENCIÓN, SE HAN PUESTO COMO LLAMADAS ASINCRONAS (CAST) PARA QUE 
+    #   FUNCIONE, PERO PROBABLEMENTE HAYA DE SER SINCRONA. ADEMÁS, 
+    #   EL IO.PUTS SOLO PRINTEA SI SE LAS LLAMA DIRECTAMENTE, EL MENSAJE
+    #   SE PIERDE EN EL MENÚ. HAY QUE RECUPERARLO.
+
+    def ver_viajes(), do: GenServer.cast(:stowserver, :viajes)
+
+    @impl true
+    def handle_cast(:viajes,state) do
+            IO.puts "Estos son los viajes que hemos encontrado:\n"
+            {:noreply, state}
+    end
+
+
+
+    def ver_historial(), do: GenServer.cast(:stowserver, :historial)
+
+    @impl true
+    def handle_cast(:historial,state) do
+            IO.puts "Tus viajes:\n"
+            {:noreply, state}
+    end
+
 
 end
