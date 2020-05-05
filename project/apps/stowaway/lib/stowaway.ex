6a6afd0c3d17d15f71ead48e5133d711caf25092
Alvaro Perez <a.perez.amado@udc.es>
2020-05-05 20:25:19 +0200

    funciona

--- project/apps/stowaway/lib/stowaway.ex
+++ project/apps/stowaway/lib/stowaway.ex
@@ -4,16 +4,16 @@ defmodule Stowaway do
   para reservar una plaza en uno de ellos, para ver su historial de viajes,
   y si quiere cancelar alguno.
   """
-  
+
     #SERVER
- 
+
     def levantar_servidor() do
         GenServer.start_link(Stowaway, [], name: :stowserver)
     end
 
     @impl true
-    def init(smth) do 
-        {:ok, smth} 
+    def init(smth) do
+        {:ok, smth}
     end
 
 
@@ -31,18 +31,19 @@ defmodule Stowaway do
     def ver_historial(login) do
         GenServer.call(:stowserver, {:historial, login})
     end
-    
+
     @impl true
     def handle_call({:historial,login}, _from, []) do
-        {:reply, filterSearch("#{login}") ,[]}
+        {:reply, filterSearch(login) ,[]}
     end
 
-    def filterSearch(login) do "../Record.csv" 
-        |> Path.expand 
-        |> File.stream! 
-        |> CSV.decode 
-        |> Enum.map(fn x -> elem(x,1) end) 
-        |> Enum.filter(&match?([_,login,_,_],&1))
+    def filterSearch(login) do
+        "../Record.csv"
+        |> Path.expand
+        |> File.stream!
+        |> CSV.decode
+        |> Enum.map(fn x -> elem(x,1) end)
+        |> Enum.filter(&match?([_,^login,_,_],&1))
     end
 
 end
