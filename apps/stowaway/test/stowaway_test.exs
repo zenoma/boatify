3b7adafb532519ef4f9fdd302168dba0fe65d6e3
Ana Maria Lema Carril <ana.lema.carril@udc.es>
2020-05-08 21:52:58 +0200

    Testing Stowaway y Directory

--- apps/stowaway/test/stowaway_test.exs
+++ apps/stowaway/test/stowaway_test.exs
@@ -2,7 +2,26 @@ defmodule StowawayTest do
   use ExUnit.Case
   doctest Stowaway
 
-  test "greets the world" do
-    assert Stowaway.hello() == :world
+  test "start_stoaway" do
+    Stowaway.start(1)
   end
+
+  test "levantar_servidor_stoaway" do
+    {_,pid} = Stowaway.levantar_servidor(:Sandra)
+    assert Process.alive?(pid) ==  true
+  end
+
+  test "parar_servidor" do
+    {_,pid} = Stowaway.levantar_servidor(:Sandra)
+    Stowaway.parar_servidor(:Sandra)
+    assert Process.alive?(pid) ==  false
+  end
+
+  test "stop_stoaway" do
+    Stowaway.start(1)
+    Stowaway.stop
+  end
+   
 end
+
+
