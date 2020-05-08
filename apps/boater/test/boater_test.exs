f5bad8ff159e0d512c0544537fe481799c71289c
Alvaro Perez <a.perez.amado@udc.es>
2020-05-08 20:59:00 +0200

    haciendo test Boater

--- apps/boater/test/boater_test.exs
+++ apps/boater/test/boater_test.exs
@@ -3,7 +3,22 @@ defmodule BoaterTest do
   doctest Boater
 
   test "levantar_servidor" do
-   {_,pid} = Boater.levantar_servidor()
+   {_,pid} = Boater.levantar_servidor(:Sandra)
    assert Process.alive?(pid) ==  true
+  end
+
+  test "parar_servidor" do
+    {_,pid} = Boater.levantar_servidor(:Sandra)
+    Boater.parar_servidor(:Sandra)
+    assert Process.alive?(pid) ==  false
+  end
+
+  test "start" do
+    Boater.start(1)
+  end
+
+  test "stop" do
+    Boater.start(1)
+    Boater.stop
   end
 end
