038446a889af4bb467c742dc8e3c1f5c9006656d
Alvaro Perez <a.perez.amado@udc.es>
2020-05-09 18:24:57 +0200

    seguimos test

--- apps/boater/test/boater_test.exs
+++ apps/boater/test/boater_test.exs
@@ -24,22 +24,22 @@ defmodule BoaterTest do
     assert Process.alive?(pid) ==  false
   end
 
-  # test "ver_viajesDisp" do
-  #   Boater.levantar_servidor(:ServerName)
-  #   assertBoater.ver_viajesDisp(:ServerName) == [
-  #     ["1", "Raquel", "Hanse -458", "22/11/2020", "Gomera – Las Palmas",
-  #      "06:20:00 AM", "6", "6", "Open"],
-  #     ["2", "Sergio", "Brava 22 W.A", "23/11/2020", "Lanzarote – FuerteVentura",
-  #      "10:20:00 PM", "5", "3", "Open"],
-  #     ["3", "Sandra", "Br 22 W.A", "23/11/2020", "Larote – FuerteVentura",
-  #      "10:20:00 PM", "5", "3", "Open"],
-  #     ["4", "Sandra", "Brava 22 W.A", "23/11/2020", "Lanzarote – FuerteVentura",
-  #      "10:20:00 PM", "5", "3", "Open"],
-  #     ["8", "John", "Yamaha", "FromHereToThere", "TheLastOneWasDate", "aBlink", "5",
-  #      "5", "Open"]
-  #   ]
+  test "ver_viajesDisp" do
+    Boater.levantar_servidor(:ServerName)
+    assert Boater.ver_viajesDisp(:ServerName) == [
+      ["1", "Raquel", "Hanse -458", "22/11/2020", "Gomera – Las Palmas",
+       "06:20:00 AM", "6", "6", "Open"],
+      ["2", "Sergio", "Brava 22 W.A", "23/11/2020", "Lanzarote – FuerteVentura",
+       "10:20:00 PM", "5", "3", "Open"],
+      ["3", "Sandra", "Br 22 W.A", "23/11/2020", "Larote – FuerteVentura",
+       "10:20:00 PM", "5", "3", "Open"],
+      ["4", "Sandra", "Brava 22 W.A", "23/11/2020", "Lanzarote – FuerteVentura",
+       "10:20:00 PM", "5", "3", "Open"],
+      ["8", "John", "Yamaha", "FromHereToThere", "TheLastOneWasDate", "aBlink", "5",
+       "5", "Open"]
+    ]
 
-  # end
+  end
   #Falla
 
 end
